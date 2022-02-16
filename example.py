6948aed46ee26d8ef007eec4274a5ccb8a5e5a14
Trevon Lawrence <trevon.lawrence@cdw.com>
2022-02-15 23:02:08 -0500

    Fifth Commit

--- example.py
+++ example.py
@@ -20,12 +20,19 @@ class MainCharacter:
 
 #Scream
 def scream():
+    system('cls')
     print("You scream as loud as you can.")
+    sleep(3)
     print("The sounds of your scream bounce around the room but to no avail.")
+    sleep(3)
     print("Your voice starts rasping due to the dry atmosphere of the room.")
+    sleep(3)
     print("Sweat beads down your forehead as you start to panic a little.")
+    sleep(3)
     print("You hear a noise coming from beyond the door.")
+    sleep(3)
     print("Quickly, you press your ear up against the wooden door.")
+    sleep(3)
     choice = int(input("What will you do?\n[0] Yell for help\n[1] Look around the room, maybe you missed something\n[2] Try breaking down the door again, it's only wood\n:"))
     if choice == 0:
         print("You chose to yell for help")
@@ -36,12 +43,19 @@ def scream():
 
 #Looking around
 def look_around():
+    system('cls')
     print("Examining the room, you notice that the rug is the only thing in the room.")
+    sleep(3)
     print("The rug itself is ugly and old, but also has some weight to it.")
+    sleep(3)
     print("You grab an end of the rug and pull as hard as you can.")
+    sleep(3)
     print("This revealed what looks like a secret hatch")
+    sleep(3)
     print("As you lay the rest of the rug down, you hear a very quiet snicker.")
+    sleep(3)
     decision = int(input("What will you do?\n[0] Keep calm and open the hatch\n[1] Look around again to make sure you didn't miss anything\n[2] Scream\n:"))
+    system('cls')
     if decision == 0:
         print("You chose to push forward and open the hatch")
     elif decision == 1:
@@ -53,12 +67,19 @@ def look_around():
 
 #Breaking the door down
 def break_door():
+    system('cls')
     mc.reduce_health(1)
+    sleep(3)
     print("You hurt your arm trying to break the door down.")
-    print(f"Your health is {mc.health}")
+    sleep(3)
+    print(f"Your health is {mc.health} HP")
+    sleep(3)
     print("The door won't budge either.")
+    sleep(3)
     print("Will you try again or look for more options?")
+    sleep(3)
     choice = int(input("What will you do?\n[0] Try breaking the door down again\n[1] Look around the room, there's bound to be something of interest\n[2] You could try screaming\n:"))
+    system('cls')
     if choice == 0:
         print("You have chosen to try again.")
         break_door()
@@ -86,6 +107,7 @@ def main():
 
 # Game Over
 def game_over():
+    system('cls')
     print("You have died")
     print("Would you like to restart?")
 # Introduction and character definition
